86868982c42c030b7355b55682c704ca36373632
PhiMaii <pmairguenther@web.de>
2025-12-12 14:12:01 +0100

    First working version

--- esp-client/Config.h
+++ esp-client/Config.h
@@ -1,6 +1,8 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#define SOFTWARE_VERSION "0.1.0"
+
 // WiFi credentials
 #define WIFI_SSID "FRITZ!Box 7590 NU"
 #define WIFI_PASS "95925916586060820281"
@@ -16,8 +18,10 @@
 #define LED_PIN LED_BUILTIN  // onboard LED
 
 // Intervals (ms)
-#define EVENT_FETCH_INTERVAL 10000UL  // 60s
-#define BLINK_POLL_INTERVAL 1000UL    // 1s
-#define REGISTER_INTERVAL 300000UL    // 5min
+#define EVENT_FETCH_INTERVAL 10UL * 1000UL  // 60s
+#define BLINK_POLL_INTERVAL 1UL * 1000UL    // 1s
+#define REGISTER_INTERVAL 5UL * 60UL * 1000UL    // 5min
+
+#define OTA_CHECK_INTERVAL 1UL * 60UL * 60UL * 1000UL // 1 h
 
 #endif
