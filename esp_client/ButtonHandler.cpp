f5bc9a903c478e20dd2e900e5c08a66f4a1c075b
PhiMaii <pmairguenther@web.de>
2025-12-15 22:17:27 +0100

    Enhance ButtonHandler to support long press detection

--- esp_client/ButtonHandler.cpp
+++ esp_client/ButtonHandler.cpp
@@ -3,7 +3,7 @@
 #include "ButtonHandler.h"
 
 ButtonHandler::ButtonHandler(uint8_t pin)
-  : _pin(pin), _lastState(HIGH), _pressed(false), _lastDebounce(0) {}
+  : _pin(pin), _lastState(HIGH), _pressed(false), _longPressed(false), _lastDebounce(0), _pressStartTime(0) {}
 
 void ButtonHandler::begin() {
   pinMode(_pin, INPUT_PULLUP);
@@ -12,18 +12,39 @@ void ButtonHandler::begin() {
 void ButtonHandler::loop() {
   bool reading = digitalRead(_pin);
 
+  // Debounce state change
   if (reading != _lastState) {
     _lastDebounce = millis();
   }
+
+  // Button pressed (HIGH->LOW transition)
   if (_lastState == HIGH && reading == LOW) {
     _pressed = true;
+    _longPressed = false;
+    _pressStartTime = millis();
   }
+
+  // Check for long press while held
+  if (_pressed && !_longPressed && (millis() - _pressStartTime > LONG_PRESS_MS)) {
+    _longPressed = true;
+    Serial.println("LONG PRESS!!!");
+  }
+
   _lastState = reading;
 }
 
 bool ButtonHandler::wasPressed() {
-  if (_pressed) {
+  if (_pressed && !_longPressed) {  // Only short press
     _pressed = false;
+    return true;
+  }
+  return false;
+}
+
+bool ButtonHandler::wasLongPressed() {
+  if (_longPressed) {
+    _longPressed = false;
+    _pressed = false;  // Clear both
     return true;
   }
   return false;
--- esp_client/ButtonHandler.h
+++ esp_client/ButtonHandler.h
@@ -11,13 +11,18 @@ public:
   void begin();
   void loop();
   bool wasPressed();
+  bool wasLongPressed();  // New: Long press (>1000ms)
 
 private:
   uint8_t _pin;
   bool _lastState;
   bool _pressed;
+  bool _longPressed;
+
   unsigned long _lastDebounce;
+  unsigned long _pressStartTime;
   const unsigned long DEBOUNCE_MS = 50;
+  const unsigned long LONG_PRESS_MS = 1000;
 };
 
 #endif
