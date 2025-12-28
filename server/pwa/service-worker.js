8dd6a6ee0ea505ec61c2d7c950ac3e6bdd852868
PhiMaii <pmairguenther@web.de>
2025-12-28 22:24:28 +0100

    Changed the style and icon of the PWA

--- server/pwa/service-worker.js
+++ server/pwa/service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener("push", event => {
 
     const options = {
         body: data.body,
-        icon: "/icon.png"
+        icon: "../config/heart.png",
     };
 
     event.waitUntil(
