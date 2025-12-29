1a6ea72ad27f37824d509eae10aadfae7b4db83f
PhiMaii <pmairguenther@web.de>
2025-12-29 23:24:39 +0100

    Planned the MQTT topic sturcture

--- server/server/mqtt.js
+++ server/server/mqtt.js
@@ -3,6 +3,91 @@ const net = require("net");
 const http = require("http");
 const ws = require("ws");
 
+/*
+MQTT Topics:
+- time2love/pair/{pairId}/blink  (published by device when blink detected)
+    - published: devices
+    - subscribed: server, devices
+
+    QoS 1
+
+    {
+    "sender": "devA",
+    "ts": 1735500000,
+    }
+
+- time2love/pair/{pairId}/event  (timestamp for the next event)
+    - published: server
+    - subscribed: devices
+
+    TODO: Maybe use http instead?
+
+##########
+
+- time2love/device/{deviceId}/config/get
+    - published: server
+    - subscribed: device
+
+    QoS 1
+
+    { "requestId": "b6c7...", "ts": 1735490000 }
+
+- time2love/device/{deviceId}/config/state
+    - published: device
+    - subscribed: server
+
+    QoS 1
+
+    {
+    "requestId": "b6c7...",
+    "ts": 1735490001,
+    "config": { "sampleRate": 10, "mode": "eco" },
+    "version": 42
+    }
+
+- time2love/device/{deviceId}/config/set
+    - published: server
+    - subscribed: device
+
+    QoS 1
+
+    {
+    "requestId": "f1a2...",
+    "ts": 1735490100,
+    "expectedVersion": 42,
+    "config": { "sampleRate": 5 }
+    }
+
+- time2love/device/{deviceId}/config/ack
+    - published: device
+    - subscribed: server
+
+    QoS 1
+
+    {
+    "requestId": "f1a2...",
+    "ok": true,
+    "newVersion": 43
+    }
+
+
+
+*/
+
+function brokerSend(topic, msg, qos = 0, retain = false) {
+    aedes.publish({
+        cmd: 'publish',
+        qos: qos,
+        retain: retain,
+        topic,
+        payload: Buffer.from(
+            typeof msg === 'string' ? msg : JSON.stringify(msg)
+        ),
+    }, (err) => {
+        if (err) console.error('broker publish error', err);
+    });
+}
+
 function start(devices, push) {
 
     // MQTT TCP
@@ -45,7 +130,7 @@ function start(devices, push) {
     aedes.on("publish", async (packet, client) => {
         if (!client) return;
 
-        console.log(`📤 PUBLISH clientId=${client.id} topic=${packet.topic}`);
+        console.log(`📤 PUBLISH clientId=${client.id} QOS=${packet.qos} Retain=${packet.retain} topic=${packet.topic} Payload=${packet.payload}`);
 
         // Detect blink messages
         const match = packet.topic.match(/^time2love\/pair\/(\d+)\/blink$/);
